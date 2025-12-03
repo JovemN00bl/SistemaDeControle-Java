8d58efdcf77516a20f9b118f385214b5a3424225
JovemN00bl <admarjunior56@gmail.com>
2025-12-03 10:27:54 -0300

    Service And Controller Client

---
+++ src/main/java/com/rodriguesadmar/controlesistema/controller/ClienteController.java
@@ -0,0 +1,79 @@
+package com.rodriguesadmar.controlesistema.controller;
+
+
+import com.rodriguesadmar.controlesistema.model.Cliente;
+import com.rodriguesadmar.controlesistema.service.ClienteService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(ClienteController.class);
+    private final ClienteService clienteService;
+
+    @GetMapping
+    public String listarClientes(Model model) {
+        List<Cliente> clientes = clienteService.findAll();
+        model.addAttribute("clientes", clientes);
+        return "clientes/lista";
+    }
+
+    @GetMapping("/novo")
+    public String novoClienteForm(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "clientes/formulario";
+    }
+
+    @GetMapping("/salvar")
+    public String salvarCliente(@Valid @ModelAttribute Cliente cliente,
+                                BindingResult result,
+                                RedirectAttributes attributes,
+                                Model model) {
+        if (result.hasErrors()) {
+            return "clientes/formulario";
+        }
+        try{
+            clienteService.save(cliente);
+            attributes.addFlashAttribute("mensagemSucesso", "Cliente salvo com Sucesso!");
+            return "redirect:/clientes";
+        } catch (DataIntegrityViolationException e) {
+            logger.error("Erro ao salvar cliente: {}", e.getMessage());
+            model.addAttribute("mensagemErro", "Erro: CPF/CNPJ ou E-mail já cadastrados no sistema.");
+             return "clientes/formulario";
+        }
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarClienteForm(@PathVariable Long id, Model model) {
+        clienteService.findById(id).ifPresent(cliente -> {model.addAttribute("cliente", cliente);});
+        return "clientes/formulario";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirCliente(@PathVariable Long id, RedirectAttributes attributes ) {
+        try {
+            clienteService.deleteById(id);
+            attributes.addFlashAttribute("mensagemSucesso", "Cliente excluido com sucesso !");
+        } catch (Exception e) {
+            attributes.addFlashAttribute("mensagemError", "Não foi possivel excluir o cliente!");
+        }
+        return "redirect:/clientes";
+
+    }
+}
---
+++ src/main/java/com/rodriguesadmar/controlesistema/service/ClienteService.java
@@ -0,0 +1,37 @@
+package com.rodriguesadmar.controlesistema.service;
+
+
+import ch.qos.logback.core.net.server.Client;
+import com.rodriguesadmar.controlesistema.model.Cliente;
+import com.rodriguesadmar.controlesistema.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> findById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente save(Cliente cliente) {
+
+        return clienteRepository.save(cliente);
+
+    }
+
+    public void deleteById(Long id) {
+         clienteRepository.deleteById(id);
+    }
+
+}
