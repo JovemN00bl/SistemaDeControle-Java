a8ae534d62f97f1137806d4e30b40866d4f1ffe6
JovemN00bl <admarjunior56@gmail.com>
2026-01-28 20:00:13 -0300

    FEAT: Vendas detalhes

--- src/main/java/com/rodriguesadmar/controlesistema/controller/VendaController.java
+++ src/main/java/com/rodriguesadmar/controlesistema/controller/VendaController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -35,6 +36,16 @@ public class VendaController {
         List<PedidoVenda> vendas = pedidoRepository.findAllByOrderByDataHoraDesc();
         model.addAttribute("vendas", vendas);
         return "vendas/lista";
+
+    }
+
+    @GetMapping("/{id}")
+    public String detalhesVenda(@PathVariable Long id, Model model) {
+        PedidoVenda pedido = pedidoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Venda invalida: " + id));
+
+        model.addAttribute("pedido", pedido);
+        return "vendas/detalhes";
 
     }
 
